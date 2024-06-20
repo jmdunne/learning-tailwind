64c779d1c02b56a66ae35ae7a75af33788372304
Josh Dunne <joshua.m.dunne@gmail.com>
2024-06-20 12:32:31 -0700

    1. Colors - Basic plugging in default and custom

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,20 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    // Import custom colors here
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        vuejs: {
+          100: "#42b883",
+          200: "#35a674",
+          300: "#289461",
+          400: "#1c824e",
+          500: "#0f703b",
+          600: "#025e28",
+          700: "#004c16",
+          800: "#003a03",
+          900: "#002700",
+        },
       },
     },
   },
